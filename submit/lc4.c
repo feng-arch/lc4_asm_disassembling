8968433f3336212f946e1ae046f84436bb7ea7a8
feng-arch <feng-arch@outlook.com>
2024-11-13 10:19:53 +0800

    add basic function, program can give a result

--- submit/lc4.c
+++ submit/lc4.c
@@ -7,49 +7,64 @@
 /************************************************************************/
 
 #include <stdio.h>
+#include <string.h>
 #include "lc4_memory.h"
 #include "lc4_loader.h"
 #include "lc4_disassembler.h"
 
 /* program to mimic pennsim loader and disassemble object files */
 
-int main (int argc, char** argv) {
-  
-/* leave plenty of room for the filename */
-  
+int main(int argc, char **argv) {
+  /* leave plenty of room for the filename */
+
   char filename[100];
 
-	/**
-	 * main() holds the linked list &
-	 * only calls functions in other files
-	 */
+  /**
+     * main() holds the linked list &
+     * only calls functions in other files
+     */
 
-	/* step 1: create head pointer to linked list: memory 	*/
-	/* do not change this line - there should no be malloc calls in main() */
-	
-	row_of_memory* memory = NULL ;
+  /* step 1: create head pointer to linked list: memory   */
+  /* do not change this line - there should no be malloc calls in main() */
 
+  row_of_memory *memory = NULL;
 
-	/* step 2: determine filename, then open it		*/
-	/*   TODO: extract filename from argv, pass it to open_file() */
+  /* step 2: determine filename, then open it     */
+  /*   TODO: extract filename from argv, pass it to open_file() */
+  if (argc < 2) {
+    printf("Usage: %s <object_file.obj>\n", argv[0]);
+    return -1;
+  }
+  strcpy(filename, argv[1]);
+  FILE *file = open_file(filename);
+  if (file == NULL) {
+    return -1;
+  }
 
+  /* step 3: call function: parse_file() in lc4_loader.c  */
+  /*   TODO: call function & check for errors     */
+  if (parse_file(file, &memory) != 0) {
+    fclose(file);
+    return -1;
+  }
+  fclose(file);
 
-	/* step 3: call function: parse_file() in lc4_loader.c 	*/
-	/*   TODO: call function & check for errors		*/
+  /* step 4: call function: reverse_assemble() in lc4_disassembler.c */
+  /*   TODO: call function & check for errors     */
+  if (reverse_assemble(memory) != 0) {
+    return -1;
+  }
 
+  /* step 5: call function: print_list() in lc4_memory.c   */
+  /*   TODO: call function                */
+  print_list(memory);
 
-	/* step 4: call function: reverse_assemble() in lc4_disassembler.c */
-	/*   TODO: call function & check for errors		*/
+  /* step 6: call function: delete_list() in lc4_memory.c */
+  /*   TODO: call function & check for errors     */
+  if (delete_list(&memory) != 0) {
+    return -1;
+  }
 
-
-	/* step 5: call function: print_list() in lc4_memory.c 	*/
-	/*   TODO: call function 				*/
-
-
-	/* step 6: call function: delete_list() in lc4_memory.c */
-	/*   TODO: call function & check for errors		*/
-
-
-	/* only return 0 if everything works properly */
-	return 0 ;
+  /* only return 0 if everything works properly */
+  return 0;
 }
--- submit/lc4_memory.c
+++ submit/lc4_memory.c
@@ -7,88 +7,127 @@
 /************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lc4_memory.h"
-
 
 /*
  * adds a new node to a linked list pointed to by head
  */
-int add_to_list (row_of_memory** head,
-		 short unsigned int address,
-		 short unsigned int contents)
-{
+int add_to_list(row_of_memory **head,
+                short unsigned int address,
+                short unsigned int contents) {
+  /* Check if the node already exists */
+  row_of_memory *existing_node = search_address(*head, address);
+  if (existing_node != NULL) {
+    existing_node->contents = contents;
+    return 0;
+  }
 
-    /* check to see if there is already an entry for this address and update the contents.  no additional steps required in this case */
-    
-    /* allocate memory for a single node */
+  /* Allocate memory for a new node */
+  row_of_memory *new_node = (row_of_memory *)malloc(sizeof(row_of_memory));
+  if (new_node == NULL) {
+    return -1;
+  }
 
-	/* populate fields in newly allocated node w/ address&contents, NULL for label and assembly */
-	/* do not malloc() storage for label and assembly here - do it in parse_file() and reverse_assemble() */
+  /* Initialize the new node */
+  new_node->address = address;
+  new_node->contents = contents;
+  new_node->label = NULL;
+  new_node->assembly = NULL;
+  new_node->next = NULL;
 
-	/* if *head is NULL, node created is the new head of the list! */
+  /* Insert the new node into the linked list in ascending order */
+  if (*head == NULL || (*head)->address > address) {
+    new_node->next = *head;
+    *head = new_node;
+  } else {
+    row_of_memory *current = *head;
+    while (current->next != NULL && current->next->address < address) {
+      current = current->next;
+    }
+    new_node->next = current->next;
+    current->next = new_node;
+  }
 
-	/* otherwise, insert node into the list in address ascending order */
-
-	/* return 0 for success, -1 if malloc fails */
-
-	return 0 ;
+  return 0;
 }
-
-
 
 /*
  * search linked list by address field, returns node if found
  */
-row_of_memory* search_address (row_of_memory* head,
-			       short unsigned int address )
-{
-	/* traverse linked list, searching each node for "address"  */
-
-	/* return pointer to node in the list if item is found */
-
-	/* return NULL if list is empty or if "address" isn't found */
-
-	return NULL ;
+row_of_memory *search_address(row_of_memory *head,
+                              short unsigned int address) {
+  row_of_memory *current = head;
+  while (current != NULL) {
+    if (current->address == address) {
+      return current;
+    }
+    current = current->next;
+  }
+  return NULL;
 }
 
 /*
  * search linked list by opcode field, returns node if found
  */
-row_of_memory* search_opcode  (row_of_memory* head,
-				      short unsigned int opcode  )
-{
-    /* opcode parameter is in the least significant 4 bits of the short int and ranges from 0-15 */
-		/* see assignment instructions for a detailed description */
-    
-    /* traverse linked list until node is found with matching opcode in the most significant 4 bits
-	   AND "assembly" field of node is NULL */
-
-	/* return pointer to node in the list if item is found */
-
-	/* return NULL if list is empty or if no matching nodes */
-
-	return NULL ;
+row_of_memory *search_opcode(row_of_memory *head,
+                             short unsigned int opcode) {
+  row_of_memory *current = head;
+  while (current != NULL) {
+    unsigned short instr = current->contents;
+    unsigned short instr_opcode = (instr >> 12) & 0xF;
+    if (instr_opcode == opcode && current->assembly == NULL) {
+      return current;
+    }
+    current = current->next;
+  }
+  return NULL;
 }
 
+void print_list(row_of_memory *head) {
+  /* make sure head isn't NULL */
+  if (head == NULL) {
+    return;
+  }
 
-void print_list (row_of_memory* head )
-{
-	/* make sure head isn't NULL */
+  /* print out a header */
+  printf("%-20s %-20s %-20s %s\n", "<label>", "<address>", "<contents>", "<assembly>");
 
-	/* print out a header */
-    
-	/* traverse linked list, print contents of each node */
+  /* traverse linked list, print contents of each node */
+  row_of_memory *current = head;
+  while (current != NULL) {
+    char address_str[20];
+    char contents_str[20];
+    sprintf(address_str, "%04X", current->address);
+    sprintf(contents_str, "%04X", current->contents);
+    printf("%-20s %-20s %-20s %s\n",
+           current->label ? current->label : "",
+           address_str,
+           contents_str,
+           current->assembly ? current->assembly : "");
+    current = current->next;
+  }
 
-	return ;
+  return;
 }
 
 /*
  * delete entire linked list
  */
-int delete_list    (row_of_memory** head )
-{
-	/* delete entire list node by node */
-	/* set the list head pointer to NULL upon deletion */
-
-	return 0 ;
+int delete_list(row_of_memory **head) {
+  row_of_memory *current = *head;
+  while (current != NULL) {
+    row_of_memory *next_node = current->next;
+    if (current->label != NULL) {
+      free(current->label);
+    }
+    if (current->assembly != NULL) {
+      free(current->assembly);
+    }
+    free(current);
+    current = next_node;
+  }
+  *head = NULL;
+  return 0;
 }
